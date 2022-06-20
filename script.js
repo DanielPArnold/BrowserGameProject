ce00636a0a46f815c5dd86fa2d6144fcaca19fc1
Daniel Arnold <danielpatrickarnold@gmail.com>
2022-06-20 12:28:09 -0700

    Added a window section for the game. Added temporary window for proof of concept.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const pc = newPlayableCharacter(100, 110)
+const canvas = document.getElementById('border');
+
+let windowHeight = window.innerHeight;
+let windowWidth = window.innerWidth;
+console.log(canvas.width);
+
+const pc = newPlayableCharacter(windowWidth/2 - 40, 65);
+
